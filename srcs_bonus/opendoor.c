65ba27fc231621d7b48cd6af011e0e0cf4103072
bdetune <25184475+soleraeus@users.noreply.github.com>
2022-06-14 12:20:16 +0200

    Norme

--- srcs_bonus/opendoor.c
+++ srcs_bonus/opendoor.c
@@ -6,59 +6,70 @@
 /*   By: tnaton <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 19:06:30 by tnaton            #+#    #+#             */
-/*   Updated: 2022/06/13 19:07:04 by tnaton           ###   ########.fr       */
+/*   Updated: 2022/06/14 12:18:21 by bdetune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void	toggle_door(t_info *info)
+static void	init_toggle_door(t_info *info, t_proj *proj)
 {
-	t_proj	proj;
+	proj->door = NULL;
+	proj->v[0] = cos(info->player.angle);
+	proj->v[1] = -sin(info->player.angle);
+	proj->cur[0] = info->player.x;
+	proj->delta[0] = 10;
+	if (proj->v[0] < -0.0001 || proj->v[0] > 0.0001)
+	{
+		if (proj->v[0] < -0.0001)
+			proj->cur[0] = floor(info->player.x) - 0.0001;
+		else
+			proj->cur[0] = ceil(info->player.x);
+		proj->delta[0] = fabs(proj->cur[0] - info->player.x) / fabs(proj->v[0]);
+	}
+	proj->cur[1] = info->player.y;
+	proj->delta[1] = 10;
+	if (proj->v[1] < -0.0001 || proj->v[1] > 0.0001)
+	{
+		if (proj->v[1] < -0.0001)
+			proj->cur[1] = floor(info->player.y) - 0.0001;
+		else
+			proj->cur[1] = ceil(info->player.y);
+		proj->delta[1] = fabs(proj->cur[1] - info->player.y) / fabs(proj->v[1]);
+	}
+}
+
+static void	door_in_range(t_info *info, t_proj *proj)
+{
 	int		coords[2];
 
-	proj.door = NULL;
-	proj.v[0] = cos(info->player.angle);
-	proj.v[1] = -sin(info->player.angle);
-	proj.cur[0] = info->player.x;
-	proj.delta[0] = 10;
-	if (proj.v[0] < -0.0001 || proj.v[0] > 0.0001)
-	{
-		if (proj.v[0] < -0.0001)
-			proj.cur[0] = floor(info->player.x) - 0.0001;
-		else
-			proj.cur[0] = ceil(info->player.x);
-		proj.delta[0] = fabs(proj.cur[0] - info->player.x) / fabs(proj.v[0]);
-	}
-	proj.cur[1] = info->player.y;
-	proj.delta[1] = 10;
-	if (proj.v[1] < -0.0001 || proj.v[1] > 0.0001)
-	{
-		if (proj.v[1] < -0.0001)
-			proj.cur[1] = floor(info->player.y) - 0.0001;
-		else
-			proj.cur[1] = ceil(info->player.y);
-		proj.delta[1] = fabs(proj.cur[1] - info->player.y) / fabs(proj.v[1]);
-	}
-	if (proj.delta[0] < proj.delta[1])
-		proj.cur[1] = proj.delta[0] * proj.v[1] + info->player.y;
+	if (proj->delta[0] < proj->delta[1])
+		proj->cur[1] = proj->delta[0] * proj->v[1] + info->player.y;
 	else
-		proj.cur[0] = proj.delta[1] * proj.v[0] + info->player.x;
-	while (distance(proj.cur[0], proj.cur[1], \
-				info->player.x, info->player.y) <= 1)
+		proj->cur[0] = proj->delta[1] * proj->v[0] + info->player.x;
+	while (distance(proj->cur[0], proj->cur[1], \
+			info->player.x, info->player.y) <= 1)
 	{
-		coords[0] = (int)proj.cur[0];
-		coords[1] = (int)proj.cur[1];
+		coords[0] = (int)proj->cur[0];
+		coords[1] = (int)proj->cur[1];
 		if (info->map[coords[1]][coords[0]] == '1')
 			return ;
 		if (info->map[coords[1]][coords[0]] == '2')
 		{
-			proj.door = find_door(info, coords);
-			break ;
+			proj->door = find_door(info, coords);
+			return ;
 		}
-		proj.cur[0] += 0.1 * proj.v[0];
-		proj.cur[1] += 0.1 * proj.v[1];
+		proj->cur[0] += 0.1 * proj->v[0];
+		proj->cur[1] += 0.1 * proj->v[1];
 	}
+}
+
+void	toggle_door(t_info *info)
+{
+	t_proj	proj;
+
+	init_toggle_door(info, &proj);
+	door_in_range(info, &proj);
 	if (proj.door)
 	{
 		proj.door->opened_time = 0;
@@ -70,6 +81,30 @@ void	toggle_door(t_info *info)
 			proj.door->motion = -1;
 		else
 			proj.door->motion = 1;
+	}
+}
+
+void	door_next_position(t_door *current)
+{
+	if (current->opened_time == 60)
+		current->motion = 1;
+	if (current->motion == -1)
+		current->visible -= 2;
+	else if (current->motion == 1)
+		current->visible += 2;
+	else if (current->visible == 0 && current->motion == 0)
+		current->opened_time += 1;
+	if (current->visible < 0)
+	{
+		current->visible = 0;
+		current->motion = 0;
+		current->opened_time = 1;
+	}
+	else if (current->visible > 100)
+	{
+		current->visible = 100;
+		current->motion = 0;
+		current->opened_time = 0;
 	}
 }
 
@@ -88,28 +123,7 @@ void	handle_doors(t_info *info)
 			current->motion = 0;
 		}
 		else
-		{
-			if (current->opened_time == 60)
-				current->motion = 1;
-			if (current->motion == -1)
-				current->visible -= 2;
-			else if (current->motion == 1)
-				current->visible += 2;
-			else if (current->visible == 0 && current->motion == 0)
-				current->opened_time += 1;
-			if (current->visible < 0)
-			{
-				current->visible = 0;
-				current->motion = 0;
-				current->opened_time = 1;
-			}
-			else if (current->visible > 100)
-			{
-				current->visible = 100;
-				current->motion = 0;
-				current->opened_time = 0;
-			}
-		}
+			door_next_position(current);
 		current = current->next;
 	}
 }
